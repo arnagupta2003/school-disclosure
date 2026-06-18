7902e71ae48d1e416db1d1de85a708fcd3fb1c28
arnagupta2003 <83420761+arnagupta2003@users.noreply.github.com>
2026-06-18 16:04:53 +0000

    Updated School details

--- config.js
+++ config.js
@@ -2,57 +2,59 @@
 const SchoolConfig = {
   // A: General Information
   generalInfo: [
-    { id: 1, info: "NAME OF THE SCHOOL", details: "YOUR SCHOOL NAME HERE" },
-    { id: 2, info: "AFFILIATION NO. (IF APPLICABLE)", details: "1234567" },
-    { id: 3, info: "SCHOOL CODE (IF APPLICABLE)", details: "10101" },
-    { id: 4, info: "COMPLETE ADDRESS WITH PIN CODE", details: "123 School Road, Sector 4, New Delhi - 110001" },
-    { id: 5, info: "PRINCIPAL NAME", details: "Dr. Jane Doe" },
-    { id: 6, info: "PRINCIPAL QUALIFICATION", details: "M.Sc., B.Ed., Ph.D." },
-    { id: 7, info: "SCHOOL EMAIL ID", details: "info@yourschool.edu.in" },
-    { id: 8, info: "CONTACT DETAILS (LANDLINE/MOBILE)", details: "+91 11 2345 6789 / +91 98765 43210" }
+    { id: 1, info: "NAME OF THE SCHOOL", details: "Vidya Bhawan Girls’ Senior Secondary School" },
+    { id: 2, info: "AFFILIATION NO.(IF APPLICABLE)", details: "2788032" },
+    { id: 3, info: "SCHOOL CODE (IF APPLICABLE)", details: "87104" },
+    { id: 4, info: "COMPLETE ADDRESS WITH PIN CODE", details: "New Rajinder Nagar, New Delhi- 110060" },
+    { id: 5, info: "VICE PRINCIPAL NAME", details: "POOJA" },
+    { id: 6, info: "VICE PRINCIPAL QUALIFICATION", details: "M.Com, M.A. (Economics), B.Ed" },
+    { id: 7, info: "SCHOOL EMAIL ID", details: "vidyabhawandelhi@gmail.com" },
+    { id: 8, info: "CONTACT DETAILS (LANDLINE/MOBILE)", details: "011-35627382 / 9212213141" }
   ],
 
   // B: Documents and Information
   documents: [
-    { id: 1, title: "COPIES OF AFFILIATION/UPGRADATION LETTER AND RECENT EXTENSION OF AFFILIATION, IF ANY", link: "https://drive.google.com/..." },
-    { id: 2, title: "COPIES OF SOCIETIES/TRUST/COMPANY REGISTRATION/RENEWAL CERTIFICATE, AS APPLICABLE", link: "https://drive.google.com/..." },
-    { id: 3, title: "COPY OF NO OBJECTION CERTIFICATE (NOC) ISSUED, IF APPLICABLE, BY THE STATE GOVT./UT", link: "https://drive.google.com/..." },
-    { id: 4, title: "COPIES OF RECOGNITION CERTIFICATE UNDER RTE ACT, 2009, AND IT'S RENEWAL IF APPLICABLE", link: "https://drive.google.com/..." },
-    { id: 5, title: "COPY OF VALID BUILDING SAFETY CERTIFICATE AS PER THE NATIONAL BUILDING CODE", link: "https://drive.google.com/..." },
-    { id: 6, title: "COPY OF VALID FIRE SAFETY CERTIFICATE ISSUED BY THE COMPETENT AUTHORITY", link: "https://drive.google.com/..." },
-    { id: 7, title: "COPY OF THE DEO CERTIFICATE SUBMITTED BY THE SCHOOL FOR AFFILIATION/UPGRADATION/EXTENSION OF AFFILIATION OR SELF CERTIFICATION BY SCHOOL", link: "https://drive.google.com/..." },
-    { id: 8, title: "COPIES OF VALID WATER, HEALTH AND SANITATION CERTIFICATES", link: "https://drive.google.com/..." }
+    { id: 1, title: "COPIES OF AFFILIATION/UPGRADATION LETTER AND RECENT EXTENSION OF AFFILIATION, IF ANY", link: "ok" },
+    { id: 2, title: "COPIES OF SOCIETIES/TRUST/COMPANY REGISTRATION/RENEWAL CERTIFICATE, AS APPLICABLE", link: "ok" },
+    { id: 3, title: "COPY OF NO OBJECTION CERTIFICATE (NOC) ISSUED, IF APPLICABLE, BY THE STATE GOVT./UT", link: "ok" },
+    { id: 4, title: "COPIES OF RECOGNITION CERTIFICATE UNDER RTE ACT, 2009, AND IT'S RENEWAL IF APPLICABLE", link: "ok" },
+    { id: 5, title: "COPY OF VALID BUILDING SAFETY CERTIFICATE AS PER THE NATIONAL BUILDING CODE", link: "Not provided" },
+    { id: 6, title: "COPY OF VALID FIRE SAFETY CERTIFICATE ISSUED BY THE COMPETENT AUTHORITY", link: "ok" },
+    { id: 7, title: "COPY OF THE DEO CERTIFICATE SUBMITTED BY THE SCHOOL FOR AFFILIATION/UPGRADATION/EXTENSION OF AFFILIATION OR SELF CERTIFICATION BY SCHOOL", link: "Ok" },
+    { id: 8, title: "COPIES OF VALID WATER, HEALTH AND SANITATION CERTIFICATES", link: "Ok" }
   ],
 
   // C: Result and Academics
   academics: [
-    { id: 1, title: "FEE STRUCTURE OF THE SCHOOL", link: "https://drive.google.com/..." },
-    { id: 2, title: "ANNUAL ACADEMIC CALENDAR", link: "https://drive.google.com/..." },
-    { id: 3, title: "LIST OF SCHOOL MANAGEMENT COMMITTEE (SMC)", link: "https://drive.google.com/..." },
-    { id: 4, title: "LIST OF PARENTS TEACHERS ASSOCIATION (PTA) MEMBERS", link: "https://drive.google.com/..." },
-    { id: 5, title: "LAST THREE-YEAR RESULT OF THE BOARD EXAMINATION AS PER APPLICABILITY", link: "https://drive.google.com/..." }
+    { id: 1, title: "FEE STRUCTURE OF THE SCHOOL", link: "There is no fee charge from any student" },
+    { id: 2, title: "ANNUAL ACADEMIC CALENDAR", link: "ok" },
+    { id: 3, title: "LIST OF SCHOOL MANAGEMENT COMMITTEE (SMC)", link: "15" },
+    { id: 4, title: "LIST OF PARENTS TEACHERS ASSOCIATION (PTA) MEMBERS", link: "10" },
+    { id: 5, title: "LAST THREE-YEAR RESULT OF THE BOARD EXAMINATION AS PER APPLICABILITY", link: "<strong>CLASS X</strong><br>(93.16%) 2023-2024<br>(68.69%) 2024-2025<br>(88.88%) 2025-2026<br><br><strong>CLASS XII</strong><br>(87.40%) 2023-2024<br>(90.68%) 2024-2025<br>(90.51%) 2025-2026" }
   ],
 
   // D: Staff (Teaching)
   staff: [
-    { id: 1, info: "PRINCIPAL", details: "Dr. Jane Doe" },
-    { id: 2, info: "TOTAL NO. OF TEACHERS", details: "45" },
-    { id: 3, info: "• PGT", details: "12" },
-    { id: 4, info: "• TGT", details: "18" },
-    { id: 5, info: "• PRT", details: "15" },
-    { id: 6, info: "TEACHERS SECTION RATIO", details: "1:1.5" },
-    { id: 7, info: "DETAILS OF SPECIAL EDUCATOR", details: "Mrs. Sarah Smith (B.Ed in Special Education)" },
-    { id: 8, info: "DETAILS OF COUNSELLOR AND WELLNESS TEACHER", details: "Mr. Alan Green (M.A. in Psychology)" }
+    { id: 1, info: "VICE PRINCIPAL", details: "1" },
+    { id: 2, info: "TOTAL NO. OF TEACHERS", details: "36" },
+    { id: 3, info: "PGT", details: "10" },
+    { id: 4, info: "TGT", details: "15" },
+    { id: 5, info: "PRT", details: "10" },
+    { id: 6, info: "PET", details: "1" },
+    { id: 7, info: "Other Staff", details: "6" },
+    { id: 8, info: "TEACHERS SECTION RATIO", details: "1:5" },
+    { id: 9, info: "DETAILS OF SPECIAL EDUCATOR", details: "NIL" },
+    { id: 10, info: "DETAILS OF COUNSELLOR AND WELLNESS TEACHER", details: "3" }
   ],
 
   // E: School Infrastructure
   infrastructure: [
-    { id: 1, info: "TOTAL CAMPUS AREA OF THE SCHOOL (IN SQ MTR)", details: "8500 SQ MTR" },
-    { id: 2, info: "NO. AND SIZE OF THE CLASS ROOMS (IN SQ MTR)", details: "35 Classrooms - 45 SQ MTR each" },
-    { id: 3, info: "NO. AND SIZE OF LABORATORIES INCLUDING COMPUTER LABS (IN SQ MTR)", details: "5 Labs - 60 SQ MTR each" },
-    { id: 4, info: "INTERNET FACILITY (YES/NO)", details: "YES" },
-    { id: 5, info: "NO. OF GIRLS TOILETS", details: "15" },
-    { id: 6, info: "NO. OF BOYS TOILETS", details: "15" },
-    { id: 7, info: "LINK OF YOUTUBE VIDEO OF THE INSPECTION OF SCHOOL COVERING THE INFRASTRUCTURE OF THE SCHOOL", details: "https://www.youtube.com/watch?v=..." }
+    { id: 1, info: "TOTAL CAMPUS AREA OF THE SCHOOL (IN SQ MTR)", details: "5956" },
+    { id: 2, info: "NO. AND SIZE OF THE CLASS ROOMS (IN SQ FT)", details: "NO. OF CLASS ROOMS SIZE 400 TO 499 sq. ft: 15<br>No. of class room size 500 sq.ft. and above: 30<br>No. of class room of size less than 400 sq ft: 17" },
+    { id: 3, info: "NO. AND SIZE OF LABORATORIES INCLUDING COMPUTER LABS (IN SQ MTR)", details: "COMPUTER LAB - 40<br>BIOLOGY LAB – 35<br>PHYSICS LAB - 54<br>CHEMISTRY LAB - 72<br>MATHS LAB – 26" },
+    { id: 4, info: "INTERNET FACILITY", details: "YES" },
+    { id: 5, info: "NO. OF GIRLS TOILETS", details: "25" },
+    { id: 6, info: "NO. OF BOYS TOILETS", details: "2" },
+    { id: 7, info: "LINK OF YOUTUBE VIDEO OF THE INSPECTION OF SCHOOL COVERING THE INFRASTRUCTURE OF THE SCHOOL", details: "Not provided" }
   ]
 };
