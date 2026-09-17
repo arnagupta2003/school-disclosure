51c05d76f371563a33822312777c88ecb7ad5040
arnagupta2003 <83420761+arnagupta2003@users.noreply.github.com>
2026-09-17 14:29:08 +0000

    Fixed Assistant Teacher count

--- config.js
+++ config.js
@@ -49,7 +49,7 @@ const SchoolConfig = {
     { id: 3, info: "PGT", details: "9" },
     { id: 4, info: "TGT", details: "14" },
     { id: 5, info: "PRT", details: "10" },
-    { id: 6, info: "Assistant Teachers", details: "0" },
+    { id: 6, info: "Assistant Teachers", details: "10" },
     { id: 7, info: "Other Staff", details: "6" },
     { id: 8, info: "TEACHERS SECTION RATIO", details: "1:5" },
     { id: 9, info: "DETAILS OF SPECIAL EDUCATOR", details: "NIL" },
